699dc034b48dc8ab59d2ab52883654d33fd8aaf1
Aaiza Khan <36559905+AaizaKhan@users.noreply.github.com>
2019-01-22 13:56:38 +0500

    Add files via upload

---
+++ Part A/task1.php
@@ -0,0 +1,16 @@
+
+<?php
+for ($i = 1 ; $i<=50; $i++) {
+if($i%15 == 0)
+   print "StarStruck"."<br>";
+elseif ($i%5 == 0)
+     print "Struck".".<br>";
+	elseif ($i%3== 0)
+    print "Star"."<br>";
+else
+   print $i."<br>";
+
+
+}
+
+?>
